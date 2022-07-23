e3fad950d3acf7eeeac33fb08562ab8317ca7960
AlanG0000 <alan.g100@yahoo.com>
2022-07-23 18:30:38 +1000

    Programming Assignment 2 upload

--- Week 3 Assignment upload.R
+++ Week 3 Assignment upload.R
@@ -1,38 +1,3 @@
-# This function takes a matrix as an input (default is a null matrix).
-# This function is capable of caching the matrix and the inverse if desired,
-# as well as retrieving both.
-
-makeCacheMatrix <- function(m = matrix()) {
-    inv <- NULL
-    mx <- NULL
-    
-    # cache the matrix, set inverse to NULL.
-    cache_mx <- function(m) {
-        mx <<- m
-        inv <<- NULL
-    }
-    
-    # create the inverse matrix, cache it and the original matrix.
-    inv_mx <- function(m) {
-        inv <<- solve(m)
-        mx <<- m
-    }
-    
-    # Retrieve the cached inverse.
-    get_inv <- function() inv
-    
-    # Retrieve the cached matrix.
-    get_mx <- function() mx
-    
-    # Return the list of available methods.
-    list(inv_mx = inv_mx, get_inv = get_inv,
-         cache_mx = cache_mx, get_mx = get_mx)
-}
-
-
-# This function will take an object that was used in the previous function as input.
-# It will check if the inverse of the matrix has already been produced and cached.
-# If it has been cached already then it will be returned, if not then it will be
 # created, cached and returned.
 cacheSolve <- function(x, ...) {
     
